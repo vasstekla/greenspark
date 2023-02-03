09e26472379d8de9146e2fc436192c09b04f40b3
Vass Tekla <vasstekla@yahoo.com>
2023-02-03 18:56:00 +0200

    Remove npx created code

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ProductPage from './components/productPage/ProductPage';
+import "@fontsource/cabin"
 
 function App() {
   return (
