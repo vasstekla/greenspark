e2601e32eb2120a9d010b1dffa203c957f7f9ec3
Vass Tekla <vasstekla@yahoo.com>
2023-02-05 14:04:54 +0200

    Add Axios package and get data from mocklab

--- ui/src/components/product/ProductPage.tsx
+++ ui/src/components/product/ProductPage.tsx
@@ -1,38 +1,19 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { IProduct } from '../../models/IProduct'
 import ProductCard from './ProductCard'
 import './ProductPage.css'
 
 export default function ProductPage() {
-    const [products, setProducts] = useState([
-        {
-            "id": 1,
-            "type": "plastic bottles",
-            "amount": 100,
-            "action": "collects",
-            "active": true,
-            "linked": false,
-            "selectedColor": "blue"
-        },
-        {
-            "id": 2,
-            "type": "trees",
-            "amount": 10,
-            "action": "plants",
-            "active": false,
-            "linked": true,
-            "selectedColor": "green"
-        },
-        {
-            "id": 3,
-            "type": "carbon",
-            "amount": 100,
-            "action": "offsets",
-            "active": false,
-            "linked": false,
-            "selectedColor": "beige"
-        }
-    ])
+    const [products, setProducts] = useState<IProduct[]>([])
+
+    useEffect(() => {
+        axios.get(`https://getgreenspark.mocklab.io/product-widgets`)
+            .then(res => {
+                const products: IProduct[] = res.data;
+                setProducts(products);
+            })
+    }, [])
 
     return (
         <>
